4fd8107446618c9e70d2cfafcfb45717c757c7e4
lukexyz <lukedwoods@gmail.com>
2021-09-30 23:24:53 +0100

    improve location feature to mood description

--- prodb/core.py
+++ prodb/core.py
@@ -18,12 +18,12 @@ from time import gmtime, strftime
 def generate_db(prefill=False):
     if prefill:
         df = pd.DataFrame({'name': ['Sam', 'Grant'],
-                           'location': ['UK', 'NZ'],
-                           'score': [0, 30],
+                           'mood': ['😊', '😵'],
+                           'message': ['hi', 'hello'],
                            'time_utc' : [arrow.utcnow().format('YYYY-MM-DD HH:mm:ss'),
                                          arrow.utcnow().format('YYYY-MM-DD HH:mm:ss')]})
     else:
-        df = pd.DataFrame(columns=['name', 'location', 'score', 'time_utc'])
+        df = pd.DataFrame(columns=['name', 'mood', 'message', 'time_utc'])
     df.to_csv('db.csv', index=None)
 
 
@@ -32,8 +32,8 @@ def generate_db(prefill=False):
 def insert_row(df, data, db_path='db.csv'):
     new_row = pd.Series({'time_utc' : arrow.utcnow().format('YYYY-MM-DD HH:mm:ss'),
                          'name':data['name'],
-                         'location': data['location'],
-                         'score': data['score']})
+                         'mood': data['mood'],
+                         'message': data['message']})
     df = df.append(new_row, ignore_index=True)
     df.to_csv(db_path, index=None)
     return df
